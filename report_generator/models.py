3eb11d5eb63a155b59eb4c2d31126a8a8e213f2f
indiasfernandes <daniel.indias@gmail.com>
2024-01-17 15:14:07 +0000

    Created function to screenshot webPage and save in a folder in project

--- report_generator/models.py
+++ report_generator/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class WebsiteReport(models.Model):
+    url = models.URLField()
+    screenshot = models.ImageField(upload_to='screenshots/')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    overall_rating = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    # Diagnostics
+    cta_button_placement_rating = models.FloatField()
+    cta_clarity_rating = models.FloatField()
+    form_simplicity_rating = models.FloatField()
+    form_autofill_rating = models.FloatField()
+    messaging_clarity_rating = models.FloatField()
+    headline_focus_rating = models.FloatField()
+    offer_transparency_rating = models.FloatField()
+
+    # Trust Signals
+    social_proof = models.TextField()  # Testimonials, reviews, etc.
+    company_info_presence = models.TextField()  # Company data, policies, etc.
---
+++ report_generator/screenshot/takeScreenshoot.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from urllib.parse import urlparse
+
+# Function to get the domain from a URL
+def get_domain_from_url(url):
+    # Parse the URL to extract the domain
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+
+    # Remove 'www.' if it exists in the domain
+    if domain.startswith('www.'):
+        domain = domain[4:]  # Remove the first 4 characters 'www.'
+
+    return domain
+
+# Function to take a screenshot of a web page
+def take_screenshot(url):
+    # Set up headless Chrome options
+    options = Options()
+    options.headless = True
+    options.add_argument("--headless")  # Ensure GUI is off
+    options.add_argument("--no-sandbox")  # Bypass OS security model
+    options.add_argument("--disable-gpu")  # Applicable to windows os only
+
+    # Initialize the driver
+    driver = webdriver.Chrome(options=options)
+
+    # Load the page
+    driver.get(url)
+
+    path = '../../exports/images/screenshots/' + get_domain_from_url(url) + '.png'
+
+    # Save the screenshot
+    driver.save_screenshot(path)
+
+    # Close the browser
+    driver.quit()
+
+take_screenshot('https://www.innerflect.com')
+
+
+
