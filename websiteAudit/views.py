162715125bf935016603ab50d7a7319bff45e5d7
indiasfernandes <daniel.indias@gmail.com>
2024-02-10 12:10:52 +0000

    Added stdout Button Clear

--- websiteAudit/views.py
+++ websiteAudit/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import os
-
 from api.forms import ContactForm
 from utils.web_scrapper import fetch_website_html
 from utils.take_screenshot import take_screenshot
 from websiteAudit import settings
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def display_stdout(request):
@@ -44,3 +45,10 @@ def web_scraper_view(request):
         content = fetch_website_html(url)  # Adjust to your actual function
         return HttpResponse(content, content_type='text/html')
     return render(request, 'web_scrapper.html')
+
+
+def clear_stdout(request):
+    log_file_path = os.path.join(settings.LOG_DIR, 'stdout.log')
+    with open(log_file_path, 'w') as log_file:
+        log_file.truncate()
+    return HttpResponseRedirect(reverse('display_stdout'))  # Redirect back to the stdout display page
