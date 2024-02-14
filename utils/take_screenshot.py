4251c966681e8429924e8a18827591c4007adebc
IndiasFernandes <daniel.indias@innerflect.com>
2024-02-14 18:18:27 +0000

    AWS chromedriver

--- utils/take_screenshot.py
+++ utils/take_screenshot.py
@@ -4,9 +4,14 @@ from urllib.parse import urlparse
 import os
 import logging
 from django.conf import settings
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 
 logger = logging.getLogger(__name__)
 
+
+ 
 # Function to get the domain from a URL
 def get_domain_from_url(url):
     parsed_url = urlparse(url)
@@ -24,17 +29,22 @@ def take_screenshot(url):
         "deviceMetrics": {"width": 375, "height": 612, "pixelRatio": 3.0},
         "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
     }
+    
 
-    # Chrome options including mobile emulation and headless mode
-    chrome_options = Options()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    chrome_options.add_argument('--headless')  # Run headless Chrome
+    
+
 
     # Specify the path to chromedriver.exe (manually download and specify the correct path)
-    chrome_driver_path = os.path.join(settings.BASE_DIR, '/screenshot/', 'chromedriver')
+    chrome_driver_path = os.path.join(settings.BASE_DIR, 'utils', 'chromedriver')
+    service = Service(executable_path=chrome_driver_path)
+    chrome_options = webdriver.ChromeOptions()
+    # Chrome options including mobile emulation and headless mode
+    
+    #chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    chrome_options.add_argument('--headless')  # Run headless Chrome
 
-    # Initialize the WebDriver with the specified ChromeDriver path
-    driver = webdriver.Chrome(options=chrome_options)
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         # Open the URL
