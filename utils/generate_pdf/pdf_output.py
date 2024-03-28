b375426db25e38620613d69ef39d4daab13a06aa
indiasfernandes <daniel.indias@gmail.com>
2024-03-28 11:51:36 +0000

    Update gitignore

--- utils/generate_pdf/pdf_output.py
+++ utils/generate_pdf/pdf_output.py
@@ -31,11 +31,30 @@ class PDFOutput(object):
         self.body_data = body_data
         self.debug = debug
 
-    def get_image(self, path, width):
+    def get_image(self, filename, max_width, max_height):
+        path = filename
         img = utils.ImageReader(path)
         iw, ih = img.getSize()
-        aspect = ih / float(iw)
-        return Image(path, width=width, height=(width * aspect), hAlign='LEFT')
+        aspect_ratio = iw / float(ih)
+
+        # Calculate the new width and height based on the aspect ratio
+        if iw > ih:
+            new_width = min(max_width, iw)
+            new_height = new_width / aspect_ratio
+        else:
+            new_height = min(max_height, ih)
+            new_width = new_height * aspect_ratio
+
+        # Further adjustment if new_height exceeds max_height
+        if new_height > max_height:
+            new_height = max_height
+            new_width = new_height * aspect_ratio
+
+        # Ensure the new dimensions do not exceed the frame's dimensions
+        new_width = min(new_width, max_width)
+        new_height = min(new_height, max_height)
+
+        return Image(path, width=new_width, height=new_height, hAlign='RIGHT')
 
     def get_data(self):
         return {
